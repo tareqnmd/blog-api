e29e548b225a0cdc2b5a1bb26f333d11549606d6
Tareq <tareqnmd@gmail.com>
2025-07-27 20:38:32 +0600

    Enhance BlogFilterDto and CreateBlogDto by adding validation rules for status, title, content, and tags; improve input handling and enforce stricter validation for blog creation.

--- src/modules/blogs/dto/blog-filter.dto.ts
+++ src/modules/blogs/dto/blog-filter.dto.ts
@@ -1,1 +1,12 @@
-export class BlogFilterDto {}
+import { IsEnum, IsOptional, IsString } from 'class-validator';
+import { BlogStatus } from '../enum/blog-status.enum';
+
+export class BlogFilterDto {
+  @IsOptional()
+  @IsEnum(BlogStatus)
+  status?: BlogStatus;
+
+  @IsOptional()
+  @IsString()
+  title?: string;
+}
--- src/modules/blogs/dto/create-blog.dto.ts
+++ src/modules/blogs/dto/create-blog.dto.ts
@@ -1,8 +1,31 @@
-import { IsNotEmpty, IsString, MinLength } from 'class-validator';
+import {
+  IsArray,
+  IsEnum,
+  IsNotEmpty,
+  IsString,
+  MinLength,
+} from 'class-validator';
+import { BlogStatus } from '../enum/blog-status.enum';
 
 export class CreateBlogDto {
   @IsNotEmpty()
   @IsString()
   @MinLength(3)
   title: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(10)
+  content: string;
+
+  @IsNotEmpty()
+  @IsArray()
+  @IsString({ each: true })
+  tags: string[];
+
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(3)
+  @IsEnum(BlogStatus)
+  status: BlogStatus;
 }
