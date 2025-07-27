43991791128eb42acae7df0043c306e15e58266c
Tareq <tareqnmd@gmail.com>
2025-07-27 21:25:24 +0600

    Integrate AuthModule into AppModule to enable authentication features across the application.

--- src/modules/root/app.module.ts
+++ src/modules/root/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from '../auth/auth.module';
 import { BlogsModule } from '../blogs/blogs.module';
 import { UsersModule } from '../users/users.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [UsersModule, BlogsModule],
+  imports: [UsersModule, BlogsModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
