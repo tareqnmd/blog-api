5c5bd2dc6a89526fd17e10a5852f74b03ced342e
Tareq <tareqnmd@gmail.com>
2025-07-31 06:07:42 +0600

    Add JSDoc comment to bootstrap function in main.ts for improved documentation clarity.

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
+/**
+ * Bootstrap the application.
+ */
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
