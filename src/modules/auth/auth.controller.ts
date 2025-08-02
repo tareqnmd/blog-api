68599d5e89a8e8383c24ba7aa7e3eeca8a1c6375
Tareq <tareqnmd@gmail.com>
2025-08-02 15:16:44 +0600

    Remove signup functionality from AuthController and AuthService, streamlining authentication methods to focus solely on sign-in operations.

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -2,7 +2,6 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 import { AuthService } from './auth.service';
 import { SigninDto } from './dto/signin.dto';
-import { SignupDto } from './dto/signup.dto';
 
 /**
  * AuthController is a controller that provides methods to sign in and sign up.
@@ -26,22 +25,5 @@ export class AuthController {
   @Post('signin')
   signin(@Body() signinDto: SigninDto) {
     return this.authService.signin(signinDto);
-  }
-
-  /**
-   * Signs up a user.
-   * @param signupDto - The user to sign up.
-   * @returns A string with the user's name and email.
-   */
-  @ApiOperation({ summary: 'Sign up' })
-  @ApiResponse({
-    status: 200,
-    content: {
-      'application/json': {},
-    },
-  })
-  @Post('signup')
-  signup(@Body() signupDto: SignupDto) {
-    return this.authService.signup(signupDto);
   }
 }
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
 import { SigninDto } from './dto/signin.dto';
-import { SignupDto } from './dto/signup.dto';
 
 /**
  * AuthService is a service that provides methods to sign in and sign up.
@@ -21,14 +20,5 @@ export class AuthService {
    */
   signin(signinDto: SigninDto) {
     return signinDto;
-  }
-
-  /**
-   * Signs up a user.
-   * @param signupDto - The user to sign up.
-   * @returns A string with the user's name and email.
-   */
-  signup(signupDto: SignupDto) {
-    return signupDto;
   }
 }
