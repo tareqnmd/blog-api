d3a5642925782cbc9f659df44080f1db866abb02
Tareq <tareqnmd@gmail.com>
2025-08-02 19:49:41 +0600

    Add TagsController and TagsService for tag management functionality, implementing CRUD operations for tags.

--- src/modules/tags/tags.controller.ts
+++ src/modules/tags/tags.controller.ts
@@ -1,4 +1,46 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { CreateTagDto } from './dto/create-tag.dto';
+import { UpdateTagDto } from './dto/update-tag.dto';
+import { TagsService } from './tags.service';
 
 @Controller('tags')
-export class TagsController {}
+export class TagsController {
+  constructor(private readonly tagsService: TagsService) {}
+
+  @Post()
+  createTag(@Body() createTagDto: CreateTagDto) {
+    return this.tagsService.createTag(createTagDto);
+  }
+
+  @Get()
+  getTags() {
+    return this.tagsService.getTags();
+  }
+
+  @Get(':id')
+  getTagById(@Param('id', ParseIntPipe) id: number) {
+    return this.tagsService.getTagById(id);
+  }
+
+  @Put(':id')
+  updateTag(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateTagDto: UpdateTagDto,
+  ) {
+    return this.tagsService.updateTag(id, updateTagDto);
+  }
+
+  @Delete(':id')
+  deleteTag(@Param('id', ParseIntPipe) id: number) {
+    return this.tagsService.deleteTag(id);
+  }
+}
---
+++ src/modules/tags/tags.service.ts
@@ -0,0 +1,43 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateTagDto } from './dto/create-tag.dto';
+import { UpdateTagDto } from './dto/update-tag.dto';
+import { Tag } from './tag.entity';
+
+@Injectable()
+export class TagsService {
+  constructor(
+    @InjectRepository(Tag)
+    private readonly tagRepository: Repository<Tag>,
+  ) {}
+
+  async createTag(createTagDto: CreateTagDto) {
+    const newTag = this.tagRepository.create(createTagDto);
+    return this.tagRepository.save(newTag);
+  }
+
+  async getTags() {
+    return this.tagRepository.find();
+  }
+
+  async getTagById(id: number) {
+    return this.tagRepository.findOne({ where: { id } });
+  }
+
+  async updateTag(id: number, updateTagDto: UpdateTagDto) {
+    const tag = await this.getTagById(id);
+    if (!tag) {
+      throw new NotFoundException('Tag not found');
+    }
+    return this.tagRepository.update(id, updateTagDto);
+  }
+
+  async deleteTag(id: number) {
+    const tag = await this.getTagById(id);
+    if (!tag) {
+      throw new NotFoundException('Tag not found');
+    }
+    return this.tagRepository.remove(tag);
+  }
+}
