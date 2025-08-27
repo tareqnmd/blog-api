c968a2f57c03b6af6061f666dbe0d3507eeccc7f
Tareq <tareqnmd@gmail.com>
2025-08-27 19:21:03 +0600

    Update JWT configuration in AuthModule and .env.development
    - Added JWT access token configuration to .env.development for improved token management.
    - Enhanced AuthModule by integrating ConfigModule and JwtModule for better JWT handling and configuration management.

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,6 +1,9 @@
 import { forwardRef, Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 import { UsersModule } from '../users/users.module';
 import { AuthController } from './auth.controller';
+import jwtConfig from './config/jwt.config';
 import { AuthService } from './provider/auth.service';
 import { BcryptProvider } from './provider/bcrypt.provider';
 import { HashingProvider } from './provider/hashing.provider';
@@ -17,6 +20,10 @@ import { SignInProvider } from './provider/sign-in.provider';
     SignInProvider,
   ],
   exports: [AuthService, HashingProvider],
-  imports: [forwardRef(() => UsersModule)],
+  imports: [
+    forwardRef(() => UsersModule),
+    ConfigModule.forFeature(jwtConfig),
+    JwtModule.registerAsync(jwtConfig.asProvider()),
+  ],
 })
 export class AuthModule {}
