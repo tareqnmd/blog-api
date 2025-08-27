931515827f041c5d8d2493fb4c05c61a4fa58973
Tareq <tareqnmd@gmail.com>
2025-08-27 21:41:50 +0600

    Add REQUEST_USER_KEY constant to constant.ts for user identification

--- src/common/constant.ts
+++ src/common/constant.ts
@@ -1,2 +1,3 @@
 export const DEFAULT_PAGINATION_PAGE = 1;
 export const DEFAULT_PAGINATION_LIMIT = 10;
+export const REQUEST_USER_KEY = 'user';
---
+++ src/modules/auth/guards/access-token/access-token.guard.ts
@@ -0,0 +1,38 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Inject,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { ConfigType } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import { REQUEST_USER_KEY } from 'src/common/constant';
+import jwtConfig from '../../config/jwt.config';
+
+@Injectable()
+export class AccessTokenGuard implements CanActivate {
+  constructor(
+    private readonly jwtService: JwtService,
+    @Inject(jwtConfig.KEY)
+    private readonly jwtConfiguration: ConfigType<typeof jwtConfig>,
+  ) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest<Request>();
+    const token = request.headers.get('authorization')?.split(' ')[1];
+    if (!token) {
+      throw new UnauthorizedException();
+    }
+    try {
+      const payload = await this.jwtService.verifyAsync(
+        token,
+        this.jwtConfiguration,
+      );
+      request[REQUEST_USER_KEY] = payload;
+    } catch {
+      throw new UnauthorizedException();
+    }
+    return true;
+  }
+}
