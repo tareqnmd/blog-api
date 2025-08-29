2e03ec2d22f058d6abc08153fde13f06214cd59b
Tareq <tareqnmd@gmail.com>
2025-08-30 00:47:20 +0600

    Refactor User decorator to enhance user retrieval and type safety
    - Updated the User decorator to accept an optional field parameter for more granular user data access.
    - Changed the request type to RequestWithUser for improved type safety and clarity in user handling.
    - Utilized REQUEST_USER_KEY for consistent user data retrieval from the request object.

--- src/modules/auth/decorators/user.decorator.ts
+++ src/modules/auth/decorators/user.decorator.ts
@@ -1,8 +1,12 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { REQUEST_USER_KEY } from 'src/common/constant';
+import { JwtPayload } from 'src/common/interfaces/jwt-payload.interface';
+import { RequestWithUser } from 'src/common/interfaces/request-with-user.interface';
 
 export const User = createParamDecorator(
-  (data: unknown, ctx: ExecutionContext) => {
-    const request = ctx.switchToHttp().getRequest();
-    return request.user;
+  (field: keyof JwtPayload | undefined, ctx: ExecutionContext) => {
+    const request = ctx.switchToHttp().getRequest<RequestWithUser>();
+    const user = request[REQUEST_USER_KEY];
+    return field ? user[field] : user;
   },
 );
