17a641d40fa40ed6e714ea1f0f3d8230efff61f8
Tareq <tareqnmd@gmail.com>
2025-08-30 17:17:51 +0600

    Add Google Auth Library and update JWT configuration for Google OAuth

--- src/modules/auth/config/jwt.config.ts
+++ src/modules/auth/config/jwt.config.ts
@@ -2,6 +2,10 @@ import { registerAs } from '@nestjs/config';
 
 export default registerAs('jwt', () => ({
   secret: process.env.JWT_TOKEN_SECRET,
+
   accessTokenTtl: parseInt(process.env.JWT_ACCESS_TOKEN_TTL ?? '3600', 10),
   refreshTokenTtl: parseInt(process.env.JWT_REFRESH_TOKEN_TTL ?? '86400', 10),
+
+  googleClientId: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
 }));
