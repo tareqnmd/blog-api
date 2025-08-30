c4b049bfbc416b010dd1a379cee387a0a32ce2f8
Tareq <tareqnmd@gmail.com>
2025-08-30 17:14:43 +0600

    Update environment configuration to include Google OAuth credentials
    - Added GOOGLE_CLIENT_ID and GOOGLE_CLIENT_SECRET to .env.development for Google authentication integration.
    - Updated env.validation.ts to require GOOGLE_CLIENT_ID and GOOGLE_CLIENT_SECRET for environment validation.

--- src/config/env.validation.ts
+++ src/config/env.validation.ts
@@ -3,6 +3,7 @@ import * as Joi from 'joi';
 export default Joi.object({
   PORT: Joi.number().required(),
   NODE_ENV: Joi.string().required(),
+
   DB_HOST: Joi.string().required(),
   DB_PORT: Joi.number().required(),
   DB_USERNAME: Joi.string().required(),
@@ -10,7 +11,11 @@ export default Joi.object({
   DB_NAME: Joi.string().required(),
   DB_SYNCHRONIZE: Joi.boolean().required(),
   DB_AUTO_LOAD_ENTITIES: Joi.boolean().required(),
+
   JWT_TOKEN_SECRET: Joi.string().required(),
   JWT_ACCESS_TOKEN_TTL: Joi.string().required(),
   JWT_REFRESH_TOKEN_TTL: Joi.string().required(),
+
+  GOOGLE_CLIENT_ID: Joi.string().required(),
+  GOOGLE_CLIENT_SECRET: Joi.string().required(),
 });
