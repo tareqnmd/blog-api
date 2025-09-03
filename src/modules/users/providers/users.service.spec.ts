205c8595ebe302941da229150ec0a248c643ab21
Tareq <tareqnmd@gmail.com>
2025-09-03 22:04:31 +0600

    Enhance user service and controller tests with additional providers
    - Updated UsersController and UsersService tests to include necessary providers for dependency injection.
    - Added mock implementations for DataSource and repository tokens to improve test isolation and reliability.
    - Ensured that the tests validate the correct instantiation of the UsersController and UsersService.

--- src/modules/users/providers/users.service.spec.ts
+++ src/modules/users/providers/users.service.spec.ts
@@ -1,8 +1,55 @@
-describe('UsersController', () => {
-  beforeEach(async () => {});
+import { Test } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
+import { User } from '../user.entity';
+import { CreateGoogleUserProvider } from './create-google-user.provider';
+import { CreateUserProvider } from './create-user.provider';
+import { FindUserByEmailProvider } from './find-user-by-email.provider';
+import { FindUserByGoogleIdProvider } from './find-user-by-google-id.provider';
+import { FindUserByIdProvider } from './find-user-by-id.provider';
+import { UsersService } from './users.service';
 
-  describe('Test Controller', () => {});
+describe('UsersService', () => {
+  let service: UsersService;
+
+  beforeEach(async () => {
+    const module = await Test.createTestingModule({
+      providers: [
+        UsersService,
+        {
+          provide: DataSource,
+          useValue: {},
+        },
+        {
+          provide: getRepositoryToken(User),
+          useValue: {},
+        },
+        {
+          provide: CreateUserProvider,
+          useValue: {},
+        },
+        {
+          provide: FindUserByEmailProvider,
+          useValue: {},
+        },
+        {
+          provide: FindUserByIdProvider,
+          useValue: {},
+        },
+        {
+          provide: FindUserByGoogleIdProvider,
+          useValue: {},
+        },
+        {
+          provide: CreateGoogleUserProvider,
+          useValue: {},
+        },
+      ],
+    }).compile();
+    service = module.get<UsersService>(UsersService);
+  });
+
   it('should be defined', () => {
-    expect(true).toBeDefined();
+    expect(service).toBeDefined();
   });
 });
--- src/modules/users/users.controller.spec.ts
+++ src/modules/users/users.controller.spec.ts
@@ -1,5 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
+import { CreateGoogleUserProvider } from './providers/create-google-user.provider';
+import { CreateUserProvider } from './providers/create-user.provider';
+import { FindUserByEmailProvider } from './providers/find-user-by-email.provider';
+import { FindUserByGoogleIdProvider } from './providers/find-user-by-google-id.provider';
+import { FindUserByIdProvider } from './providers/find-user-by-id.provider';
 import { UsersService } from './providers/users.service';
+import { User } from './user.entity';
 import { UsersController } from './users.controller';
 
 describe('UsersController', () => {
@@ -8,12 +16,41 @@ describe('UsersController', () => {
   beforeEach(async () => {
     const usersModule: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        {
+          provide: DataSource,
+          useValue: {},
+        },
+        {
+          provide: getRepositoryToken(User),
+          useValue: {},
+        },
+        {
+          provide: CreateUserProvider,
+          useValue: {},
+        },
+        {
+          provide: FindUserByEmailProvider,
+          useValue: {},
+        },
+        {
+          provide: FindUserByIdProvider,
+          useValue: {},
+        },
+        {
+          provide: FindUserByGoogleIdProvider,
+          useValue: {},
+        },
+        {
+          provide: CreateGoogleUserProvider,
+          useValue: {},
+        },
+      ],
     }).compile();
     usersController = usersModule.get<UsersController>(UsersController);
   });
 
-  describe('Test Controller', () => {});
   it('should be defined', () => {
     expect(usersController).toBeDefined();
   });
