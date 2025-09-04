bb5f5e32f05fc2ce7c0ba6e37bdb0c367e885d61
Tareq <tareqnmd@gmail.com>
2025-09-04 23:37:05 +0600

    Enhance CreateUserProvider tests with mock implementations
    - Introduced mock repository for user testing to improve isolation and reliability.
    - Updated MailService and HashingProvider mocks to simulate email sending and password hashing.
    - Ensured comprehensive setup for CreateUserProvider tests, enhancing overall test coverage.

--- src/modules/users/providers/create-user.provider.service.ts
+++ src/modules/users/providers/create-user.provider.service.ts
@@ -1,13 +1,34 @@
 import { Test } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
-import { DataSource } from 'typeorm';
+import { HashingProvider } from 'src/modules/auth/providers/hashing.provider';
+import { MailService } from 'src/modules/mail/providers/mail.service';
+import { DataSource, ObjectLiteral, Repository } from 'typeorm';
 import { User } from '../user.entity';
 import { CreateUserProvider } from './create-user.provider';
-import { MailService } from 'src/modules/mail/providers/mail.service';
-import { HashingProvider } from 'src/modules/auth/providers/hashing.provider';
+
+type MockRepository<T extends ObjectLiteral = any> = Partial<
+  Record<keyof Repository<T>, jest.Mock>
+>;
+
+const createMockRepository = <
+  T extends ObjectLiteral = any,
+>(): MockRepository<T> => {
+  return {
+    create: jest.fn(),
+    save: jest.fn(),
+    findOne: jest.fn(),
+  };
+};
 
 describe('CreateUserProvider', () => {
   let provider: CreateUserProvider;
+  let userRepository: MockRepository;
+  const user = {
+    firstName: 'John',
+    lastName: 'Doe',
+    email: 'john.doe@example.com',
+    password: 'password',
+  };
 
   beforeEach(async () => {
     const module = await Test.createTestingModule({
@@ -23,15 +44,21 @@ describe('CreateUserProvider', () => {
         },
         {
           provide: MailService,
-          useValue: {},
+          useValue: {
+            welcomeEmail: jest.fn(() => Promise.resolve()),
+            sendEmail: jest.fn(() => Promise.resolve()),
+          },
         },
         {
           provide: HashingProvider,
-          useValue: {},
+          useValue: {
+            hashPassword: jest.fn(() => Promise.resolve(user.password)),
+          },
         },
       ],
     }).compile();
     provider = module.get<CreateUserProvider>(CreateUserProvider);
+    userRepository = module.get<MockRepository<User>>(getRepositoryToken(User));
   });
 
   it('should be defined', () => {
